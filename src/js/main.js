cbe071c72f28eca789f143bbb8d17a41e5c2a188
jamesonamadeus <jamesonamadeus@gmail.com>
2018-04-29 13:55:59 -0400

    add in brgr script. logo adjustment

--- src/js/main.js
+++ src/js/main.js
@@ -11,13 +11,50 @@ window.onscroll = function() {
   }
 };
 
-//lozad
-// Initialize library
-lozad(".lozad", {
-  load: function(el) {
-    el.src = el.dataset.src;
-    el.onload = function() {
-      el.classList.add("fade");
-    };
+(function() {
+  //nav brgr
+  var body = document.body;
+  var linkToggle = document.getElementsByClassName("link-togg")[0];
+  var navToggle = document.getElementsByClassName("nav-togg")[0];
+  var burgerMenu = document.getElementsByClassName("nav-menu")[0];
+  var burgerContain = document.getElementsByClassName("nav-contain")[0];
+  var burgerNav = document.getElementsByClassName("nav-list")[0];
+
+  if (document.addEventListener) {
+    document.addEventListener("click", handleClick, false);
+  } else if (document.attachEvent) {
+    document.attachEvent("onclick", handleClick);
   }
-}).observe();
+
+  function handleClick(event) {
+    event = event || window.event;
+    event.target = event.target || event.srcElement;
+
+    var element = event.target;
+
+    while (element) {
+      if (/nav-togg/.test(element.className)) {
+        toggle(element);
+        break;
+      }
+
+      element = element.parentNode;
+    }
+  }
+
+  function toggle() {
+    [body, burgerContain, burgerNav].forEach(function(el) {
+      el.classList.toggle("open");
+    });
+  }
+
+  // Lozad - Initialize library
+  lozad(".lozad", {
+    load: function(el) {
+      el.src = el.dataset.src;
+      el.onload = function() {
+        el.classList.add("fade");
+      };
+    }
+  }).observe();
+})();
