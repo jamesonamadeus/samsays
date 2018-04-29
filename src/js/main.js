a8f7618baf7266484549e9625912ad432311e8fc
jamesonamadeus <jamesonamadeus@gmail.com>
2018-04-29 13:49:05 -0400

    update the navigation

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,16 @@
+//Change Navbar Background on Scroll
+window.onscroll = function() {
+  var bnr = document.querySelector("#banner");
+
+  if (window.pageYOffset >= 100) {
+    bnr.classList.add("banner--scroll");
+    bnr.classList.remove("banner--top");
+  } else {
+    bnr.classList.add("banner--top");
+    bnr.classList.remove("banner--scroll");
+  }
+};
+
 //lozad
 // Initialize library
 lozad(".lozad", {
